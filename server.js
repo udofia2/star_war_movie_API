a70a78aef05f986a58f7346f41901cdc53cffc09
Abasiodiong Udofia <enalsy22@gmail.com>
2021-09-29 05:57:52 +0100

    added 404 error

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ app.get('/', (req, res) => {
 
   res.status(200).json({
     status: "success",
+    data: "sorry no movie here",
     msg: `Please use the endpoints /api/v1/films or /api/v1/ilm/:filmID or /api/v1/film/character/?sort=value to have access to the movies`,
-    data: "sorry no movie here"
   })
 })
 
+app.use("*", (req, res) => {
+  res.status(400).json({
+    status: 'fail',
+    message: ` Oopse, you hit the wrong route. visit https://metacare-films/api/v1/films to get your first data. cheers `
+  })
+})
 
 app.listen(process.env.PORT, () => console.log(`Server is runing, \nvisit http://${process.env.HOST_LOCAL}:${process.env.PORT}`))
